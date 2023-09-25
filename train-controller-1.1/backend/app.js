77605b2b90b4771bf2d3370e3ee384d7037c473a
Alice Olsson <aliceolsson@airsomthoralice.mynet>
2023-09-25 14:16:54 +0200

    .

--- train-controller-1.1/backend/app.js
+++ train-controller-1.1/backend/app.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
+const bodyParser = require("body-parser");
+// app.use(express.json());
+
+const express = require("express");
+const cors = require('cors');
+const morgan = require('morgan');
 const bodyParser = require('body-parser')
 
 const fetchTrainPositions = require('./models/trains.js')
