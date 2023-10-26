e83246776b5bf5e772231e4d2933074b8eef9acd
Alice Olsson <aliceolsson@airsomthoralice.mynet>
2023-10-26 09:55:16 +0200

    Lade till routes för login vyn så vi kan POST till login med email  och lösenord

--- train-controller-1.1/backend/app.js
+++ train-controller-1.1/backend/app.js
@@ -12,15 +12,19 @@ const delayed = require('./routes/delayed.js');
 const tickets = require('./routes/tickets.js');
 const codes = require('./routes/codes.js');
 const trains = require('./routes/trains.js');
+const auth = require("./routes/auth.js");
+const authModel = require("./models/auth.js");
+const database = require("../backend/db/database.js")
 
 
 const app = express()
 const httpServer = require("http").createServer(app);
 const db =  require('./db/database.js');
 
-// const databas = db.openDb()
-// const currentDatabaseName = databas.databaseName;
-// console.log(currentDatabaseName);
+const jwt = require("jsonwebtoken");
+const { error } = require('console');
+const jwtSecret = process.env.JWT_SECRET;
+
 
 
 app.use(express.json()); //Felet uppstod med "undefinied req.body var för att vi hade kommenterat ut denna, om vi vill använda oss av req.body måste vi inkludera denna express.json för att tolka JSON-data i inkommande förfrågningar"
@@ -29,9 +33,6 @@ app.options('*', cors());
 app.disable('x-powered-by');
 
 app.use(cors()); // Enable CORS for Express routes
-
-
-//const io = socketIo(server);
 
 
 const io = require("socket.io")(httpServer, {
@@ -72,18 +73,78 @@ if (process.env.NODE_ENV !== 'test') {
 //MIDDLEWARE-CORS OCH LOGGNING
 // This is middleware called for all routes.
 // Middleware takes three parameters.
-app.use((req, res, next) => {
-    console.log(req.method);
-    console.log(req.path);
-    next();
-});
 
+app.use((req, res, next) => {
+      next(); // Token är giltig, fortsätt till nästa middleware eller rutt
+    });
 
 app.get("/", (req, res) => {
     // Define the response for the root URL
     res.json({ message: "Welcome to the server!" });
   });
+
+// Middleware för att verifiera JWT-token
+function verifyToken(req, res, next) {
+    const token = req.header('x-access-token'); // om tokenen skickas som en header
+    console.log(token, "token")
   
+    if (!token) {
+      return res.status(401).json({ error: 'Åtkomst nekad: Ingen token tillhandahållen' });
+    }
+  
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Ogiltig token' });
+      }
+      next(); // Token är giltig, fortsätt till nästa middleware eller rutt
+    });
+  }
+  
+// Skyddad sökväg
+app.get('/main', verifyToken, (req, res) => {
+    res.json({ message: 'Du är inloggad och har åtkomst till /main.' });
+});
+
+// Inlogg-routerna (GET och POST)
+app.get('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const { users } = await database.openDb();
+        const user = await users.findOne({ email: email, password: password });
+
+        if (user) {
+            res.json({ user: user });
+        } else {
+            console.log(error)
+        }
+    } catch (error) {
+        console.error('Error while accessing the users collection:', error);
+        console.log(error)
+    }
+});
+
+  app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password)
+  
+    try {
+        const { users } = await database.openDb();
+        const oneUser = await users.findOne({ email, password });
+    
+      if (!oneUser) {
+        return res.status(401).json({ error: "Ogiltiga inloggningsuppgifter" });
+      }
+  
+      res.json({ message: "Inloggningen lyckades" });
+    } catch (error) {
+      console.error("Databasfel:", error);
+    }
+  });
+
+  
+  
+  
+
 // Add a route - uses send
 // app.get("/", (req, res) => {
 //     res.send("Hello World");
@@ -165,7 +226,9 @@ app.use("/tickets", tickets);
 app.use("/tickets", tickets);
 app.use("/codes", codes);
 app.use("/trains", trains);
+app.use("/login", auth);
 
+//app.all('*', authModel.checkAPIKey);
 
 /* app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -202,4 +265,5 @@ const server = httpServer.listen(port, ()=> {
 });
 
 fetchTrainPositions(io);
+
 module.exports = server;
