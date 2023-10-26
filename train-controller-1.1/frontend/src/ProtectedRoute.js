458fe8568674eb2e1e59239e49db2ee3531084f4
Alice Olsson <aliceolsson@airsomthoralice.mynet>
2023-10-26 10:04:03 +0200

    Här har vi vår protected route för våra vyer

---
+++ train-controller-1.1/frontend/src/ProtectedRoute.js
@@ -0,0 +1,20 @@
+// ProtectedRoute.js
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, authenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => 
+        authenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
