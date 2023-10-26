0955ce26eaa9bbe5d4ff4e297cf48d85c889a338
Alice Olsson <aliceolsson@airsomthoralice.mynet>
2023-10-26 09:57:36 +0200

    Ändrade i babel config för det ska fungera med våra jest tester

--- train-controller-1.1/frontend/babel.config.cjs
+++ train-controller-1.1/frontend/babel.config.cjs
@@ -1,8 +1,7 @@
 module.exports = {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      '@babel/preset-react',
-    ],
-    plugins: ["@babel/plugin-transform-arrow-functions", "transform-es2015-modules-commonjs"]
-  };
-  
+  presets: [
+    ['@babel/preset-env'],
+    '@babel/preset-react',
+  ],
+  plugins: ["@babel/plugin-transform-arrow-functions", "transform-es2015-modules-commonjs"]
+};
