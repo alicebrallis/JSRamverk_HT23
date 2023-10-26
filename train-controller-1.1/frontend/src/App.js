f96236db6d175133a853ba908a0642ab9f6ea769
Alice Olsson <aliceolsson@airsomthoralice.mynet>
2023-10-26 09:58:51 +0200

    Kommenterade ut denna för vi använder inte den

--- train-controller-1.1/frontend/src/App.js
+++ train-controller-1.1/frontend/src/App.js
@@ -1,21 +1,39 @@
-import React, { useEffect } from 'react';
-import io from 'socket.io-client';
+// import React, { useEffect } from 'react';
+// import io from 'socket.io-client';
 
-function App() {
-  useEffect(() => {
-    const socket = io('http://localhost:3000'); // Anslut till din server
+// function App() {
+//   useEffect(() => {
+//     const socket = io('http://localhost:3000'); // Anslut till din server
 
-    // Lyssna på meddelanden från servern
-    socket.on('message', (message) => {
-      console.log(`Meddelande från servern: ${message}`);
-    });
-  }, []);
+//     // Lyssna på meddelanden från servern
+//     socket.on('message', (message) => {
+//       console.log(`Meddelande från servern: ${message}`);
+//     });
+//   }, []);
 
-  return (
-    <div className="App">
-      {/* Din React-kod här */}
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       {/* Din React-kod här */}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Login from './Login';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/login" component={Login} />
+//         {/* Lägg till andra vägar här */}
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
